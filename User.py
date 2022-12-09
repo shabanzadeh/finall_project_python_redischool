f338b4509fbac878f65c4a5e5aa21902fd82c223
maryam <shabanzadeh1712@yahoo.com>
2022-12-09 20:12:26 +0100

    added price and input

--- User.py
+++ User.py
@@ -1,24 +1,67 @@
 class User:
-    def __init__(self,name, user_email, password, number_family,select_car):
+    def __init__(self,name, user_email, password,numberOffamily,select_car_user):
         self.name = name
         self.email=user_email
         self.password=password
-        self.number_family =number_family
-        self.select_car = select_car
-
+        self.numberOffamily=numberOffamily
+        self.id_booking = 0
+        self.select_car_user = select_car_user
+        self.result=[]
+    def number_family(self):
+        for i in range(1):
+            family=input("are you family: ja oder nein: ")
+            print(family)
+            if family=="nein":
+                break
+            else:
+                numberOf_family=input("how many people did you consist of: ")
+                self.numberOffamily=numberOf_family
     def change_password(self,new_password):
         self.password=new_password
-    def get_select_car(self,select_car_user):
-        self.select_car = select_car_user
+    def get_select_car(self,which_auto):
+        self.select_car = which_auto
+    def get_select_car(self):
+        neu_select_car_user= input(" Please choose the car you want: ")
+        self.select_car_user=neu_select_car_user
     def get_user_info(self):
-        print(f"User {self.name} current have a {self.number_family}")
+        print(f"User {self.name} current have a {self.numberOffamily} and chose this car {self.select_car_user}")
     def get_calculate_price(self,price):
         print(price)
     def get_calculate_auto_rabbatt(self):
-        if (self.number_family>=3):
-            self.price = self.price/3.3
+        if self.numberOffamily>=2:
+            self.price = self.price * 0.3
         else:
             self.price = self.price
+            return self.price
+    def anzahl_booking(self):
+        if self.numberOffamily>4 or self.select_car_user == "":
+            anzahle_auto = 2
+            return anzahle_auto
+
+        else:
+            anzahle_auto= 1
+            return anzahle_auto
+
+
+    def exist_booking_auto(self,anzahle_auto,total_numberCar_list):
+
+        for x in total_numberCar_list:
+            if anzahle_auto>=2 or total_numberCar_list>=2:
+                result =total_numberCar_list[0] or total_numberCar_list[2]  or total_numberCar_list[3]
+                return self.result.append(result)
+            elif total_numberCar_list>=1:
+                result=total_numberCar_list[0]
+                return self.result.append(result)
+
+    def booking_info(self):
+        print(f"haben für dich auto {self.result} zur verfügung")
+
+    # def arrange_booking(self,anzahl_ auto):
+
+
+
+
+
 
 
 
--- main.py
+++ main.py
@@ -2,35 +2,72 @@ import openpyxl
 
 from User import User
 
+
 car_file =openpyxl.load_workbook("Car_Rantal.xlsx")
 
 car_list =car_file['Tabelle1']
 #how many type car is available
 car_per_name = {}
-car_per_passagiere = {}
+car_per_passagiere={}
+car_price_type = {}
+dicount_car={}
+
+
 print(car_list.max_row)
 for car_row in range(2, car_list.max_row + 1):
-    type_car = car_list.cell(car_row,5).value
     number_passengers = car_list.cell(car_row, 3).value
+    preis_car=car_list.cell(car_row,7).value
+    type_car =car_list.cell(car_row,5).value
+
+
+
+
    # print(type_car)
     if type_car in car_per_name:
         current_name_product= car_per_name[type_car]
         car_per_name[type_car]=current_name_product +1
+
 # calculate type with number of passengers
         car_per_passagiere[type_car]=number_passengers
     else:
         car_per_name[type_car] =1
-#print(car_per_name)
-print("Herzlichen welcomen in Car rental")
+    if type_car in car_price_type:
+        current_total_price =car_price_type.get(type_car)
+        car_price_type[type_car]=preis_car
+        dicount_car[type_car]=0.3*preis_car
+    car_price_type[type_car]=preis_car
+    dicount_car[type_car]=0.3 * preis_car
 
-print(f"see available taxis at the moment with copacity: {car_per_passagiere}")
+
+#type_car_list =[]
+#for i in total_numberCar_dic:
+ #   type_car_list.append(i)
+
+
+
+
+print("Herzlichen welcomen in Car rental\n")
+
+print(f"see available taxis at the moment with copacity: {car_per_passagiere}\n")
+print(f"Car rental price in one day {car_price_type}\n")
+print(f"if the car is reserved as a family, we have a 30% discount for you: {dicount_car} Euro \n")
+
+#which_auto =input("which auto: ")
+#number_family =int(input("how many number of family: "))
+
+
+
 # how many type car with number of passasigere
 
 
 
-app_user_one =User("shabanzade","maryam shabanzadeh","pwd1","5","BMV")
+app_user_one =User("shabanzade","maryam","pwd1",0,"Hatchback")
+app_user_one.numberOffamily()
+app_user_one.get_select_car()
 app_user_one.get_user_info()
 app_user_one.change_password("234")
+
+
 import pandas as pd
 #import matplotlib.pyplot as plt
 
